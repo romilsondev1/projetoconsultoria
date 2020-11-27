a880489164ce22450049275790df83ebfedea747
romilsondev1 <74967899+romilsondev1@users.noreply.github.com>
2020-11-27 16:22:55 -0300

    update teste

---
+++ javascript/function.js
@@ -0,0 +1,11 @@
+function tabela(){
+    var nome= "romilson";
+    var idade= 25;
+    var sexo= "M";
+    var profissão= "Engenheiro";
+
+    document.getElementById("nome").innerHTML = nome;
+    document.getElementById("idade").innerHTML = idade;
+    document.getElementById("M").innerHTML = sexo;
+    document.getElementById("profissao").innerHTML = profissão;
+}
