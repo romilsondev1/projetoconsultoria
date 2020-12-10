8d34a4c114a9e8bb4232ce543588e077e05b52c6
romilsondev1 <74967899+romilsondev1@users.noreply.github.com>
2020-12-10 09:33:28 -0300

    up

--- javascript/function.js
+++ javascript/function.js
@@ -1,3 +1,2 @@
-/*window.alert ('Sejá bem vindo, você irá se surpreender')
-var nome = window.prompt('Qual é o seu nome?')
-window.confirm('Estamos preparando o ambiente para você, certo?')*/
+var nome = window.prompt('Para uma melhor interação nos informe o seu nome, por gentileza!')
+
