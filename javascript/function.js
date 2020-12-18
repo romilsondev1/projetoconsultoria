3f0cd8fe575c7c3851402bd27280604835f429f5
romilsondev1 <74967899+romilsondev1@users.noreply.github.com>
2020-12-18 09:41:13 -0300

    atualização

--- javascript/function.js
+++ javascript/function.js
@@ -1,4 +1,12 @@
-//var nome = window.prompt('Para uma melhor interação nos informe o seu nome, por gentileza!')
-
-
 //<script src="//code.jivosite.com/widget/gz6ecJuvMj" async></script>
+
+
+function newsletter (){
+    var email = window.document.getElementById('fname')
+    var msg = window.document.getElementById('information')
+    var inform= window.document.getElementById('information1')
+    msg.innerHTML=('Você foi cadastrado com sucesso!')
+    msg.style = ('text-align: center; font-size:30px;')
+    inform.innerHTML = ('OBRIGADO...')
+    inform.style = ('color: #e75500')
+}
