175f5bae426a917db5a280a52a18ef1bbc3e426f
romilsondev1 <74967899+romilsondev1@users.noreply.github.com>
2021-07-10 00:48:27 -0300

    sub

--- envio.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-if{mail(trim($_POST('email')), trim($_POST('nome')), trim($_POST('numero'),"from:romilsoncavalcante8@gmail.com"))}{
-    $resultado = "sua mensagem foi enviada"
-}else{
-    $resultado = "não foi enviado"
-}
-
-<?php echo resultado?>
